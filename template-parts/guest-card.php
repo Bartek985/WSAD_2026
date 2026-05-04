ce8059cd5da14c8bc50a69b413ea7869e883cebf
Kuba Dyrcz <60853017+Dyrczowsky@users.noreply.github.com>
2026-05-04 12:54:50 +0200

    Update surname

--- template-parts/guest-card.php
+++ template-parts/guest-card.php
@@ -6,14 +6,19 @@ $photo_link = wp_get_attachment_image_url($guest['photo'], 'full');
 
 ?>
 
-<div class="col-md-6  mb-5 m-auto pb-5">
+<div class="col-md-6 col-6 mb-5 m-auto pb-5">
 
     <?php get_template_part('template-parts/image-circle', null, array('photo' => $guest['photo'])); ?>
 
 
-    <h4><strong><?php echo esc_html($guest['title']) . ' ' . esc_html($guest['name']); ?></strong></h4>
-    <h6 class="mb-0" style="font-weight: normal;"><?php echo esc_html($guest['job']); ?></h6>
-
+    <h4>
+        <strong>
+            <?php echo esc_html($guest['title']) . ' ' . esc_html($guest['name']); ?>
+        </strong>
+    </h4>
+    <h6 class="mb-0" style="font-weight: normal;">
+        <?php echo esc_html($guest['job']); ?>
+    </h6>
     <span class="pt-5 d-block" style="font-size: 0.9em;">
         <?php echo esc_html($guest['description']); ?>
     </span>
